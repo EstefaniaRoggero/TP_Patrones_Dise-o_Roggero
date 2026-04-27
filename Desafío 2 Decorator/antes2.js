1541646b467ff5042df41fa91cf46e09259c5398
EstefaniaRoggero <roggeroestefania@gmail.com>
2026-04-27 17:43:49 -0300

    Agrego un salto de línea y un comentario al final del archivo antes2.js

--- Desafío 2 Decorator/antes2.js
+++ Desafío 2 Decorator/antes2.js
@@ -19,3 +19,5 @@ const pizza3 = new PizzaConQuesoyPepperoni();
 console.log("Simple:", pizza1.getPrecio());
 console.log("Con queso:", pizza2.getPrecio());
 console.log("Queso + pepperoni:", pizza3.getPrecio());
+
+//comentario
